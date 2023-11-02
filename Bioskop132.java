3825b9da50cbfab6ce991888fe75e13a1d067be8
Gilangp <gilangpurnomo0505@gmail.com>
2023-11-02 16:17:23 +0700

    commit semua

--- Bioskop132.java
+++ Bioskop132.java
@@ -26,10 +26,22 @@ public class Bioskop132 {
         //     System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
         // }
 
-        for (String[] barisPenonton : penonton) {
-            System.out.println("Panjang baris: " + barisPenonton.length);
-        }
+        //for (String[] barisPenonton : penonton) {
+        //    System.out.println("Panjang baris: " + barisPenonton.length);
+        //}
 
-        
+        //System.out.println("Penonton pada baris ke-3: ");
+        //for (int i = 0; i < penonton[2].length; i++) {
+        //    System.out.println(penonton[2][i]);
+        //}
+
+        //System.out.println("Penonton pada baris ke-3: ");
+        //for (String i : penonton[2]) {
+        //    System.out.println(i);
+        //}
+
+        for ( int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
+        }
     }
 }
